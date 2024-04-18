6fa415cda5af9891308da15c78f40ac17b9c6449
Jessika Solleder <jessikasolleder@u.boisestate.edu>
2024-04-18 01:02:19 -0600

    progress

--- Project5.py
+++ Project5.py
@@ -1,3 +1,9 @@
 # Constants
-Ls = 40.02 #feet
-Wm = 
+Ls = 40.02 # feet, notes
+Wm = 12966.48 # lb, see notes
+c = 45 #lb/ ft^2
+height = 20 #feet
+failure_plane_incline = 30 # degrees
+slope_face_incline = 40 # degrees
+phi = 28 # degrees
+gamma = 120 #lb/ft^3
